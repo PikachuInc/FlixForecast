3365ad8c758e476d8896086fed23bf5d3a8d4b5a
Dominic Kenny <dominicjkenny@gmail.com>
2023-11-21 20:36:34 -0500

    more components Co-authored-by: Swarna  <swarna2072@users.noreply.github.com> Co-authored-by: johnlcos <johnlcos@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import './App.css';
 import { useAppDispatch, useAppSelector } from './hooks';
+import { Router, Route, Routes } from 'react-router';
+import Dashboard from './containers/Dashboard';
+import LogInSignUp from './containers/logInSignUp';
+import { BrowserRouter } from 'react-router-dom';
 
 const App = () => {
+  const dispatch = useAppDispatch();
+  const loggedIn: boolean = useAppSelector((state) => state.users.loggedIn);
+  
   return (
-    <main>
-      <
-    </main>
+    <Routes>
+      <Route path="/" element={!loggedIn && <LogInSignUp />} />
+      <Route path="/" element={loggedIn && <Dashboard />} />
+    </Routes>
   )
 }
 
